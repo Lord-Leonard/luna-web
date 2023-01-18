a1ba59148c52224be97550998e8212a3a9a4ca88
Lord-Leonard <leonardgeorg@gmx.de>
2023-01-18 09:16:03 +0100

    new beginning

--- src/lib/trpc/middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-import { trpc } from '$lib/trpc/trpc'
-import {TRPCError} from '@trpx/server'
-
-export const auth = trpc.middleware(async ({next, ctx}) => {
-    if (!ctx.userId) throw TRPCError({code: 'UNAUTHORIZED'})
-    return next();
-})
--- src/lib/trpc/middleware/logger.ts
+++
@@ -1,9 +0,0 @@
-import { trpc } from '$lib/trpc/trpc';
-
-export const logger = trpc.middleware(async ({ path, type, next }) => {
-    const start = Date.now();
-    const result = await next();
-    const ms = Date.now() - start;
-    console.log(`${result.ok ? 'OK' : 'ERR'} ${type} ${path} - ${ms}ms`);
-    return result;
-});
